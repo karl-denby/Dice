008ce1acbd330861366ba6cd832643b0a39d15d8
Karl Denby <karl.denby@gmail.com>
2016-02-20 12:24:08 +0000

    Start working on Dice becoming individual pieces of code that return data!

---
+++ app/src/main/java/com/example/kdenby/dice/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.kdenby.dice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button bRoll;
+    ImageView imgDieOne, imgDieTwo;
+    TextView txtResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupWidgets();         // Call our super function that gets us going
+        bRoll.callOnClick();    // Roll the dice so we don't start with 12 :)
+    }
+
+    private int setupDieOne () {
+        // Write code here to do the following
+        // 1. Randomly select a number between 1 + 6
+        // 2. Set the dice image to the corresponding number
+        // 3. Return the number so the program knows
+
+        return 6;
+    }
+
+    private void setupWidgets () {
+        // Bind Java variables to XML widgets
+        bRoll = (Button)findViewById(R.id.bRoll);
+        imgDieOne = (ImageView)findViewById(R.id.imgDieOne);
+        imgDieTwo = (ImageView)findViewById(R.id.imgDieTwo);
+        txtResult = (TextView)findViewById(R.id.txtResult);
+
+        // Create some code for when the roll button is clicked
+        //  We should move this out somewhere else later.
+        bRoll.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Declare some variables and initialize them
+                int result1, result2, total;
+                Random die1, die2;
+
+                die1 = new Random();
+                die2 = new Random();
+
+                // Get 2 randoms between 0 and 5, then add 1 to each
+                result1 = die1.nextInt(6) + 1;
+                result2 = die2.nextInt(6) + 1;
+
+                // Calculate total and update txtResult.text
+                total = result1 + result2;
+                txtResult.setText("Result is: " + total);
+
+                // Update die1
+                System.out.println("die 1 is: " + result1);
+                switch (result1) {
+                    case 1:
+                        imgDieOne.setImageResource(R.drawable.die1);
+                        break;
+                    case 2:
+                        imgDieOne.setImageResource(R.drawable.die2);
+                        break;
+                    case 3:
+                        imgDieOne.setImageResource(R.drawable.die3);
+                        break;
+                    case 4:
+                        imgDieOne.setImageResource(R.drawable.die4);
+                        break;
+                    case 5:
+                        imgDieOne.setImageResource(R.drawable.die5);
+                        break;
+                    case 6:
+                        imgDieOne.setImageResource(R.drawable.die6);
+                        break;
+                }
+
+                System.out.println("die 2 is: " + result2);
+                // Update die2
+                switch (result2) {
+                    case 1:
+                        imgDieTwo.setImageResource(R.drawable.die1);
+                        break;
+                    case 2:
+                        imgDieTwo.setImageResource(R.drawable.die2);
+                        break;
+                    case 3:
+                        imgDieTwo.setImageResource(R.drawable.die3);
+                        break;
+                    case 4:
+                        imgDieTwo.setImageResource(R.drawable.die4);
+                        break;
+                    case 5:
+                        imgDieTwo.setImageResource(R.drawable.die5);
+                        break;
+                    case 6:
+                        imgDieTwo.setImageResource(R.drawable.die6);
+                        break;
+                }
+            }
+        });
+    }
+
+
+}
