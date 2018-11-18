dded4d1c32aefe7822d35717eabfa53913169a6c
karl-denby <karl.denby@gmail.com>
2018-11-18 16:24:25 +0000

    clean up - mongodb initalized once - replaceOne is changed to updateOne - Anoymous functions replaced with lambda

--- app/src/main/java/com/karldenby/dice/MainActivity.java
+++ app/src/main/java/com/karldenby/dice/MainActivity.java
@@ -1,8 +1,7 @@
 package com.karldenby.dice;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -10,43 +9,24 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Set;
-
-import android.content.Context;
-import android.content.SharedPreferences;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.UpdateOptions;
-import com.mongodb.lang.NonNull;
 import com.mongodb.stitch.android.core.Stitch;
 import com.mongodb.stitch.android.core.StitchAppClient;
-import com.mongodb.stitch.android.core.auth.StitchUser;
 import com.mongodb.stitch.android.services.mongodb.local.LocalMongoDbService;
-import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoClient;
-import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoCollection;
-import com.mongodb.stitch.core.auth.providers.anonymous.AnonymousCredential;
-import com.mongodb.stitch.core.services.mongodb.remote.RemoteUpdateOptions;
-import com.mongodb.stitch.core.services.mongodb.remote.sync.ChangeEventListener;
-import com.mongodb.stitch.core.services.mongodb.remote.sync.DefaultSyncConflictResolvers;
-import com.mongodb.stitch.core.services.mongodb.remote.sync.ErrorListener;
-import com.mongodb.stitch.core.services.mongodb.remote.sync.internal.ChangeEvent;
 
-import org.bson.BsonString;
-import org.bson.BsonValue;
+import java.util.Random;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 
 import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Updates.combine;
+import static com.mongodb.client.model.Updates.currentDate;
+import static com.mongodb.client.model.Updates.set;
+
 
 public class MainActivity extends AppCompatActivity {
-
-    // For Save/Load
-    Context context = MainActivity.this;
 
     // Variable for widget references
     Button bRoll;
@@ -75,25 +55,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        setupWidgets();         // attached activity elements to callbacks
+        setupWidgets();
 
-        mongoMobileLocalDatabaseConnection();
-        // Create the default Stitch Client
-        final StitchAppClient client =
-                Stitch.getDefaultAppClient();
+        // Create the default Stitch Client --------------------------------------------------------
+        final StitchAppClient client;
+
+        if (Stitch.hasAppClient("dice-mgatp")) {
+            client = Stitch.getDefaultAppClient();
+        } else {
+            client = Stitch.initializeDefaultAppClient("dice-mgatp");
+        }
 
         // Create a Client for MongoDB Mobile (initializing MongoDB Mobile)
-        final MongoClient mobileClient =
-                client.getServiceClient(LocalMongoDbService.clientFactory);
+        final MongoClient mobileClient = client.getServiceClient(LocalMongoDbService.clientFactory);
 
         // Point to the target collection and insert a document
         MongoCollection<Document> localCollection =
                 mobileClient.getDatabase("mm_db").getCollection("mm_col");
-
         Document doc = localCollection.find(new Document("_id", 1)).first();
-
         Log.d("mobile", mobileClient.toString());
 
+        // -----------------------------------------------------------------------------------------
 
         if (doc == null) {
             die1 = 0;
@@ -115,10 +97,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        // We should have code here that drops usage of things like cameras and sensors
-        // Nothing to heavy though as we will still be visible
 
-        // Create the default Stitch Client
+        // Create the default Stitch Client --------------------------------------------------------
         final StitchAppClient client =
                 Stitch.getDefaultAppClient();
 
@@ -130,35 +110,15 @@ public class MainActivity extends AppCompatActivity {
         MongoCollection<Document> localCollection =
                 mobileClient.getDatabase("mm_db").getCollection("mm_col");
 
-        Document document = new Document("_id", 1)
-                .append("die1", die1)
-                .append("die2", die2);
-
-        localCollection.replaceOne(new Document("_id", 1 ), document, new UpdateOptions().upsert(true));
+        localCollection.updateOne(
+                eq("_id", 1),
+                combine(set("die1", die1), set("die2", die2)));
+        // -----------------------------------------------------------------------------------------
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        // We should have teardown code here, the heavy stuff including writing to storage
-    }
-
-    private void mongoMobileLocalDatabaseConnection() {
-
-        final StitchAppClient client;
-        final MongoClient mobileClient;
-
-        // MongoDB Mobile Local
-        // Create the default Stitch Client
-        if (Stitch.hasAppClient("dice-mgatp")) {
-            client = Stitch.getDefaultAppClient();
-        } else {
-            client = Stitch.initializeDefaultAppClient("dice-mgatp");
-        }
-        mobileClient = client.getServiceClient(LocalMongoDbService.clientFactory);
-
-        Log.d("MongoMobile","Connect Setup");
-        Log.d("MongoMobile", client.toString());
     }
 
     private int setupDie (ImageView imgDie, int DieValue) {
@@ -199,64 +159,41 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
 
-        // Return the result
         return result;
     }
 
     private void setupWidgets () {
         // Bind Java variables to XML widgets
-        bRoll = (Button)findViewById(R.id.bRoll);
-        imgDieOne = (ImageView)findViewById(R.id.imgDieOne);
-        imgDieTwo = (ImageView)findViewById(R.id.imgDieTwo);
-        txtResult = (TextView)findViewById(R.id.txtResult);
+        bRoll = findViewById(R.id.bRoll);
+        imgDieOne = findViewById(R.id.imgDieOne);
+        imgDieTwo = findViewById(R.id.imgDieTwo);
+        txtResult = findViewById(R.id.txtResult);
 
-        // create some code for when the user clicks on some of these widgets
         // -- Dice 1
-        imgDieOne.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int result;
-
-                result = setupDie(imgDieOne, 6);
-                die1 = 6;
-                total = die1 + die2;
-                txtResult.setText(getString(R.string.Result,result));
-
-                result -= 1;
-            }
+        imgDieOne.setOnClickListener((View v) -> {
+            setupDie(imgDieOne, 6);
+            die1 = 6;
+            total = die1 + die2;
+            txtResult.setText(getString(R.string.Result, total));
         });
 
         // -- Dice 2
-        imgDieTwo.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int result;
-
-                result = setupDie(imgDieTwo, 6);
-                die2 = 6;
-                total = die1 + die2;
-                txtResult.setText(getString(R.string.Result, total));
-
-                result -= 1;
-            }
+        imgDieTwo.setOnClickListener((View v) -> {
+            setupDie(imgDieTwo, 6);
+            die2 = 6;
+            total = die1 + die2;
+            txtResult.setText(getString(R.string.Result, total));
         });
 
         // -- Roll button
-        bRoll.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Declare some variables and initialize them
+        bRoll.setOnClickListener((View v) -> {
+            // Get 2 randoms between 0 and 5, then add 1 to each
+            die1 = setupDie(imgDieOne, 0);
+            die2 = setupDie(imgDieTwo, 0);
 
-                // Get 2 randoms between 0 and 5, then add 1 to each
-                die1 = setupDie(imgDieOne, 0);
-                die2 = setupDie(imgDieTwo, 0);
-
-                // Calculate total and update txtResult.text
-                total = die1 + die2;
-                txtResult.setText(getString(R.string.Result, total));
-            }
+            // Calculate total and update txtResult.text
+            total = die1 + die2;
+            txtResult.setText(getString(R.string.Result, total));
         });
-
     }
-
 }
